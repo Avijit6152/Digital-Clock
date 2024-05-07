1aa9175233e44b501f1b51a5569cd8f1fbac6c4f
Avijit6152 <avijitkumarlenka1997@outlook.com>
2024-05-07 15:42:33 +0530

    modify

--- script.js
+++ script.js
@@ -39,3 +39,43 @@ function showTime() {
 }
 
 showTime();
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	var currentDate = new Date();
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	var formattedDate = currentDate.toLocaleDateString('en-US', options);
+	document.getElementById('date').innerHTML = formattedDate;
+  });
+
+  
+
+
+
+
+  // Get current weather
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var lat = 12.971599;
+    var lon = 77.594566;
+    var apiKey = '7a8cf79f1cb245fc4735b860e74fe450'; // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+	
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        var weatherDescription = data.weather[0].description;
+        var temperature = data.main.temp;
+        var weatherInfo = `Weather: ${weatherDescription}, Temperature: ${temperature}°C `;
+        document.getElementById('weather').innerHTML = weatherInfo;
+      })
+      .catch(error => {
+        console.error('Error fetching weather data:', error);
+        document.getElementById('weather').innerHTML = 'Unable to fetch weather data';
+      });
+  }, function(error) {
+    console.error('Error getting geolocation:', error);
+    document.getElementById('weather').innerHTML = 'Unable to determine location';
+  });
